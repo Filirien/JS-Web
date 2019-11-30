518c5f67d63d50edfee491fd8e22829285d9be28
Filirien <filirien@abv.bg>
2019-11-30 15:49:05 +0200

    Shop-API

---
+++ shop-api-react/server/controllers/index.js
@@ -0,0 +1,11 @@
+const categories = require('./categories')
+const users = require('./users')
+const products = require('./products')
+const comments = require('./comments')
+
+module.exports = {
+  categories,
+  users,
+  products,
+  comments
+}
---
+++ shop-api-react/server/data/seed-data.js
@@ -0,0 +1,83 @@
+const categoriesArr = [
+  { name: `Women's Fashion` },
+  { name: `Men's Fashion` },
+  { name: 'Phones & Telecommunications' },
+  { name: 'Computer, Office & Security' },
+  { name: 'Jewelry & Watches' },
+  { name: 'Bags & Shoes' },
+  { name: 'Toys, Kids & Babies' },
+  { name: 'Sports' },
+  { name: 'Home Improvement & Tools' }
+]
+
+const productsArr = [
+  {
+    name: 'Elegant Dress Plaid',
+    categoryName: `Women's Fashion`,
+    price: 79.99,
+    img: 'https://ae01.alicdn.com/kf/H70d334fdda48498e91dd016b68d7e0d0I.jpg_480x480q90.jpg_.webp',
+    description: '2019 Elegant Dress Plaid Print Summer Dresses Women White And Black Plaid Long Sleeve Mid-calf Length Loose Dress Large Size',
+    additionalInformation: [
+      'Washing Instructions: Detailed washing instructions. For examples, washing with temperature not above 30, do not use bleach, hang dry, do not iron, do not dry clean, etc.'
+    ]
+  },
+  {
+    name: 'Half Sleeve Bodycon',
+    categoryName: `Women's Fashion`,
+    price: 49.99,
+    img: 'https://ae01.alicdn.com/kf/HTB18c1HLcfpK1RjSZFOq6y6nFXap.jpg',
+    description: 'Women Dress 2019 New Fashion Half Sleeve Bodycon Elegant Office Dress Summer Knee-Length For Female Party Dresses S-XXL Size',
+    additionalInformation: [
+      'Washing Instructions: Detailed washing instructions. For examples, washing with temperature not above 30, do not use bleach, hang dry, do not iron, do not dry clean, etc.'
+    ]
+  },
+  {
+    name: ' Six Pack Abs Muscle T Shirt',
+    categoryName: `Men's Fashion`,
+    price: 79.99,
+    img: 'https://ae01.alicdn.com/kf/HTB1RG5iRpYqK1RjSZLeq6zXppXaT.jpg',
+    description: 'Big Boobs Sexy Stomach Six Pack Abs Muscle T Shirt Tops Naked Personality Novelty T-Shirts for Men Women Sexy Man tshirt homme',
+    additionalInformation: [
+      'Washing Instructions: Detailed washing instructions. For examples, washing with temperature not above 30, do not use bleach, hang dry, do not iron, do not dry clean, etc.'
+    ]
+  },
+  {
+    name: 'Goose Ga Ga Ga T shirt',
+    categoryName: `Men's Fashion`,
+    price: 19.99,
+    img: 'https://ae01.alicdn.com/kf/H70d334fdda48498e91dd016b68d7e0d0I.jpg_480x480q90.jpg_.webp',
+    description: `Men's Funny Two Goose Ga Ga Ga T shirt Unisex Short Sleeve O-Neck Harajuku Tshirt Skateboard White Causal T-Shirt`,
+    additionalInformation: [
+      'Washing Instructions: Detailed washing instructions. For examples, washing with temperature not above 30, do not use bleach, hang dry, do not iron, do not dry clean, etc.'
+    ]
+  },
+  {
+    name: 'F1 Ayrton Senna T shirt',
+    categoryName: `Men's Fashion`,
+    price: 19.99,
+    img: 'https://ae01.alicdn.com/kf/HTB10NhEOiLaK1RjSZFxq6ymPFXaA.jpg',
+    description: 'All F1 Ayrton Senna sennacars men t shirt Cars Fans male cool T-shirt Slim Fit white fitness Casual Tops tee shirt homme camisa',
+    additionalInformation: [
+      'Washing Instructions: Detailed washing instructions. For examples, washing with temperature not above 30, do not use bleach, hang dry, do not iron, do not dry clean, etc.'
+    ]
+  },
+]
+
+let commentsArr = [
+  {
+    text: 'Looks amasing!',
+    productId: '5b49ee1a6ac39b274d120ed2',
+    author: 'admin'
+  },
+  {
+    text: 'Classic',
+    productId: '5b49ee1a6ac39b274d120ed5',
+    author: 'admin'
+  }
+]
+
+module.exports = {
+  categoriesArr,
+  productsArr,
+  commentsArr
+}
